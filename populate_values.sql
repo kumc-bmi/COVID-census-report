0c1c28f355a9c189c8525502f8f6b3f48738027c
Nathan Hensel <nathanihensel@gmail.com>
2020-04-02 18:35:18 -0500

    add missing parenthesis

--- populate_values.sql
+++ populate_values.sql
@@ -2,4 +2,5 @@ update covid_report SET covidpend_icu_bed = (
   select value from redcap_data where record = @institution and field_name=covidpend_icu_bed and project_id='16558' and instance = (
     select max(instance) from redcap_data where project_id='16558' and field_name=covidpend_icu_bed and record = @institution
   )
+)
 where site_id = @institution ;
