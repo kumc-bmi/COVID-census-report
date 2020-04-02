c464bfe9bd12ccb92a40d907b5e80b7320707ecc
Nathan Hensel <nathanihensel@gmail.com>
2020-04-02 18:28:11 -0500

    revert field param

--- populate_values.sql
+++ populate_values.sql
@@ -1,5 +1,5 @@
-update covid_report SET @field_arg = (
-  select value from redcap_data where record = @institution and field_name=@field_arg and project_id='16558' and instance = (
-    select max(instance) from redcap_data where project_id='16558' and field_name=@field_arg and record = @institution
+update covid_report SET covidpend_icu_bed = (
+  select value from redcap_data where record = @institution and field_name=covidpend_icu_bed and project_id='16558' and instance = (
+    select max(instance) from redcap_data where project_id='16558' and field_name=covidpend_icu_bed and record = @institution
   )
 where site_id = @institution ;
