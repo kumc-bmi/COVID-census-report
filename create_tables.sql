ea803f1d7e7e51ffb27f0285dda0024189bf982b
willsvankumc <54042608+willsvankumc@users.noreply.github.com>
2020-04-03 12:30:28 -0500

    add olathe

--- create_tables.sql
+++ create_tables.sql
@@ -32,6 +32,7 @@ UPDATE covid_report SET site_name = 'Other Site Liberty Hospital' WHERE site_id 
 UPDATE covid_report SET site_name = 'Overland Park Regional Medical Center' WHERE site_id = '9';
 UPDATE covid_report SET site_name = 'Childrens Mercy' WHERE site_id = '10';
 UPDATE covid_report SET site_name = 'Overland Park Regional Medical Center' WHERE site_id = '11';
-UPDATE covid_report SET site_name = 'AdventHealth, Shawnee Mission' WHERE site_id = '12';
+UPDATE covid_report SET site_name = 'AdventHealth Shawnee Mission' WHERE site_id = '12';
 UPDATE covid_report SET site_name = 'Lees Summit Medical Center' WHERE site_id = '15';
 UPDATE covid_report SET site_name = 'Kansas City VA Medical Center' WHERE site_id = '16';
+UPDATE covid_report SET site_name = 'Olathe Medical Center' WHERE site_id = '18';
