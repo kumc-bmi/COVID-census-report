daaec441f0856c26dad3a8e4aa7a5d283c44f6d4
willsvankumc <54042608+willsvankumc@users.noreply.github.com>
2020-04-03 09:41:58 -0500

    null handling

--- populate_values.sql
+++ populate_values.sql
@@ -1,37 +1,42 @@
 update covid_report SET covidpend_icu_bed = (
-  select value from redcap_data where record = @institution and field_name='covidpend_icu_bed' and project_id='16558' and instance = (
-    select max(instance) from redcap_data where project_id='16558' and field_name='covidpend_icu_bed' and record = @institution
+  select value from redcap_data where record = @institution and field_name='covidpend_icu_bed' and project_id='16558' and
+  (instance IS NULL OR instance =  (
+    select max(instance) from redcap_data where project_id='16558' and field_name='covidpend_icu_bed' and record = @institution)
   )
 )
 where site_id = @institution ;
-
 update covid_report SET covidpend_icu_bed_vent = (
-  select value from redcap_data where record = @institution and field_name='covidpend_icu_bed_vent' and project_id='16558' and instance = (
-    select max(instance) from redcap_data where project_id='16558' and field_name='covidpend_icu_bed_vent' and record = @institution
+  select value from redcap_data where record = @institution and field_name='covidpend_icu_bed_vent' and project_id='16558' and
+  (instance IS NULL OR instance =  (
+    select max(instance) from redcap_data where project_id='16558' and field_name='covidpend_icu_bed_vent' and record = @institution)
   )
 )
 where site_id = @institution ;
 update covid_report SET covidcfrm_icu_bed = (
-  select value from redcap_data where record = @institution and field_name='covidcfrm_icu_bed' and project_id='16558' and instance = (
-    select max(instance) from redcap_data where project_id='16558' and field_name='covidcfrm_icu_bed' and record = @institution
+  select value from redcap_data where record = @institution and field_name='covidcfrm_icu_bed' and project_id='16558' and
+  (instance IS NULL OR instance =  (
+    select max(instance) from redcap_data where project_id='16558' and field_name='covidcfrm_icu_bed' and record = @institution)
   )
 )
 where site_id = @institution ;
 update covid_report SET covidcfrm_icu_bed_vent = (
-  select value from redcap_data where record = @institution and field_name='covidcfrm_icu_bed_vent' and project_id='16558' and instance = (
-    select max(instance) from redcap_data where project_id='16558' and field_name='covidcfrm_icu_bed_vent' and record = @institution
+  select value from redcap_data where record = @institution and field_name='covidcfrm_icu_bed_vent' and project_id='16558' and
+  (instance IS NULL OR instance =  (
+    select max(instance) from redcap_data where project_id='16558' and field_name='covidcfrm_icu_bed_vent' and record = @institution)
   )
 )
 where site_id = @institution ;
 update covid_report SET noncovid_icu = (
-  select value from redcap_data where record = @institution and field_name='noncovid_icu' and project_id='16558' and instance = (
-    select max(instance) from redcap_data where project_id='16558' and field_name='noncovid_icu' and record = @institution
+  select value from redcap_data where record = @institution and field_name='noncovid_icu' and project_id='16558' and
+  (instance IS NULL OR instance =  (
+    select max(instance) from redcap_data where project_id='16558' and field_name='noncovid_icu' and record = @institution)
   )
 )
 where site_id = @institution ;
 update covid_report SET noncovid_icu_vent = (
-  select value from redcap_data where record = @institution and field_name='noncovid_icu_vent' and project_id='16558' and instance = (
-    select max(instance) from redcap_data where project_id='16558' and field_name='noncovid_icu_vent' and record = @institution
+  select value from redcap_data where record = @institution and field_name='noncovid_icu_vent' and project_id='16558' and
+  (instance IS NULL OR instance =  (
+    select max(instance) from redcap_data where project_id='16558' and field_name='noncovid_icu_vent' and record = @institution)
   )
 )
 where site_id = @institution ;
